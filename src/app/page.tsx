fef4fa0acde0c565c9d1dbd6dda8742fb7399528
artemis prime <artemisprimedev@gmail.com>
2023-10-25 05:22:55 -0700

    previous

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React  from 'react'
 
-import AssetBanner from '@/components/block/asset-banner-block'
+import AssetBannerComponent from '@/components/block/asset-banner-block'
 import Footer from '@/components/footer'
 
 import { landing } from '@/content'
@@ -9,7 +9,7 @@ const Page: React.FC = () => (<>
   <div className=' flex flex-col justify-center items-center pb-6 pt-4'>
   {landing.assets.map((asset, index) => (
     <section key={index} className="typography dark:typography-invert flex flex-col justify-center items-center gap-6 pb-10 mb-12 pt-4">
-      <AssetBanner asset={asset} />
+      <AssetBannerComponent asset={asset} />
     </section>
   ))}
     <div className='typography dark:typography-invert flex flex-col justify-center items-center' >
