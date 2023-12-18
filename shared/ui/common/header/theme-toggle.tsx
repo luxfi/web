833d0e3af4f74794034cc560b097b76f9ce793cb
artemis prime <artemisprimedev@gmail.com>
2023-12-18 12:32:58 -0800

    fixed dependencies

--- shared/ui/common/header/theme-toggle.tsx
+++ shared/ui/common/header/theme-toggle.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
-import { Button } from '@luxdefi/ui'
+import { Button } from '../../primitives'
 
 const ThemeToggle: React.FC = () => {
 
