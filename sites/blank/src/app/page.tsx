6948c06d9cf6a0dd42d00c5c3048ef5a740c7331
artemis prime <artemisprimedev@gmail.com>
2023-12-19 17:26:39 -0800

    blank conforms to new conf scheme

--- sites/blank/src/app/page.tsx
+++ sites/blank/src/app/page.tsx
@@ -2,27 +2,22 @@ import React  from 'react'
 import { Footer, Main } from '@luxdefi/ui/common'
 import { ApplyTypography } from '@luxdefi/ui'
 
+/* for example, as slug
 type Props = {
   params: { slug: string }
   searchParams?: { [key: string]: string | string[] | undefined }
 }
+*/ 
+import conf from '../conf/site'
 
-export async function generateMetadata({ params}: Props) {
-  return { title: 'home' }
-}
+const UniversalPage = (/* { params, searchParams }: Props */) => (<>
+  <Main className='md:flex-row md:gap-4 '>
+    <ApplyTypography>
+      <h1>Empty Page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </ApplyTypography>
+  </Main>
+  <Footer conf={conf} className='max-w-screen-2xl w-full pt-16 lg:mx-auto ' />
+</>)
 
-const HomePage = ({ params, searchParams }: Props) => {
-
-
-  return (<>
-    <Main className='md:flex-row md:gap-4 '>
-      <ApplyTypography>
-        <h1>Empty Page</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </ApplyTypography>
-    </Main>
-    <Footer className='max-w-screen-2xl w-full pt-16 lg:mx-auto ' />
-  </>)
-}
-
-export default HomePage
+export default UniversalPage
---
+++ sites/blank/src/conf/site.ts
@@ -0,0 +1,16 @@
+import { LinkDef, SiteConf } from '@luxdefi/ui/types'
+import { short, aux, full } from '@luxdefi/ui/conf/main-nav'
+import footer from '@luxdefi/ui/conf/footer'
+
+export default {
+  title: 'Lux Blank',
+  template: `Lux Blank %s`,
+  desc: 'The coolest site since sliced bread.',
+  currentAs: 'https://lux.blank',
+  mainNav: {
+    short,
+    aux,
+    full
+  },
+  footer  
+} as SiteConf
