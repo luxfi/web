ea4558ba48634aec5b1a94b17669c5b8137e1149
artem ash <artemisprimedev@gmail.com>
2024-02-03 11:57:43 -0800

    removed unused 'sites' within real 'sites'

--- sites/sites/blank/src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from 'next/server'
-
-import determineDeviceMiddleware from '@luxdefi/ui/next/determine-device-middleware'
-
-export const middleware = 
-  async (request: NextRequest) => (determineDeviceMiddleware(request))
