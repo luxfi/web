cf343ecddc7cd1f8b4e5b20c6b896eeee5ac1d00
artem ash <artemisprimedev@gmail.com>
2024-02-04 20:31:24 -0800

    credit: ui@0.2.14: taking advantage of ElementBlock   xxs font size, and Image improvements market: minor cleanups in product-detail-block

--- sites/credit/src/content/index.tsx
+++ sites/credit/src/content/index.tsx
@@ -33,15 +33,19 @@ export default [
             variant: 'primary',
           },
         ]
-          } as CTABlock
+      } as CTABlock,
+      {
+        blockType: 'element',
+        element: <p className='text-xxs italic'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      }
     ],
     contentRight: [
         // https://nextjs.org/docs/app/api-reference/components/image#responsive-image-with-fill
       {
         blockType: 'image',
         src: '/assets/img/cards-1483x1361.png',
+        alt: 'cards',
         props: {
-          alt: 'cards',
           sizes: '900px',
           fill: true,
           objectFit: 'contain'
