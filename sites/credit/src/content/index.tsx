fcb1d700936411658c59abb915bfea6518416ad8
artem ash <artemisprimedev@gmail.com>
2024-02-06 00:08:52 -0800

    Fixed various issues with ElementsTableComp ui@.0.3.1:   Employed new agent support in BannerVideoComponent to    improve mobile substantially   credit: Improved content and rendering related to images

--- sites/credit/src/content/index.tsx
+++ sites/credit/src/content/index.tsx
@@ -1,14 +1,31 @@
-import type { HeadingBlock, CTABlock, ImageBlock } from '@luxdefi/ui/blocks'
+import React from 'react'
+import Image from 'next/image'
+
+import type { HeadingBlock, CTABlock, ImageBlock, ElementBlock, VideoBlock } from '@luxdefi/ui/blocks'
 import { DEF_VIDEO_PROPS } from '@luxdefi/ui/util'
 import { ApplyTypography } from '@luxdefi/ui/primitives'
 
 import type BannerVideoBlock from '@/blocks/def/banner-video'
 import type ElementTableBlock from '@/blocks/def/element-table'
 
+
+const CardImage: React.FC = () => (
+  <Image src='/assets/img/cards-1483x1361.png' alt='cards' width={150} height={137.5} loading='lazy' />
+)
+
+const fireworks = {
+  blockType: 'video',
+  videoProps: DEF_VIDEO_PROPS, 
+  poster: '/assets/video/fireworks-poster-comp.png',
+  sources: ['/assets/video/fireworks-md.mp4'],
+  dim: {md: {w: 1920, h: 1080}},
+} as VideoBlock
+
 export default [
   {
     blockType: 'banner-video',
     specifiers: 'right top text-align-right',
+    mobileFavors: 'right',
     content: [
       {
         blockType: 'heading',
@@ -21,7 +38,7 @@ export default [
       } as HeadingBlock,
       {
         blockType: 'cta',
-        specifiers: 'left',
+        specifiers: 'left 2-cols-on-mobile',
         elements: [
           {
             title: "View Benefits",
@@ -49,33 +66,36 @@ export default [
         blockType: 'image',
         src: '/assets/img/cards-1483x1361.png',
         alt: 'cards',
+        dim: {w: 1920, h: 1080},
+        fullWidthOnMobile: true,
         props: {
           sizes: '900px',
           fill: true,
-          objectFit: 'contain'
+          objectFit: 'contain',
         }
       } as ImageBlock,
     ],
-    video: {
+    video: fireworks, /*{
       blockType: 'video',
       videoProps: DEF_VIDEO_PROPS, 
-      poster: '/assets/video/coral-poster-comp.png',
-      sources: ['/assets/video/coral-md.mp4'],
+      //poster: '/assets/video/coral-poster-comp.png',
+      //sources: ['/assets/video/coral-md.mp4'],
       dim: {md: {w: 1920, h: 1080}},
-    }
+    }*/
   },
   {
     blockType: 'banner-video',
+    specifiers: 'center top narrow-gutters constrain-to-lg',
     content: [
       {
         blockType: 'element-table',
         elements: [
           [
-            {blockType: 'element', element: '(Card Image)'},
-            {blockType: 'element', element: '(Darker Cards)'},
-            {blockType: 'element', element: '(Silver + Gold)'},
-            {blockType: 'element', element: '(Hologram Cards)'},
-            {blockType: 'element', element: '(Titanium Cards)'},
+            {blockType: 'element', element: 'Cards'},
+            {blockType: 'element', element: <CardImage />},
+            {blockType: 'element', element: <CardImage />},
+            {blockType: 'element', element: <CardImage />},
+            {blockType: 'element', element: <CardImage />},
           ],
           [
             {blockType: 'element', element: 'Level'},
@@ -114,18 +134,22 @@ export default [
           ],
         ]
       } as ElementTableBlock,
+      { blockType: 'space' },
       {
         blockType: 'element',
-        element: <ApplyTypography className='typography-h6:text-center'><h6>Open the door to a universe of digital assets with the Lux Card, serving as your key to effortless transactions and proficient asset management within the Lux ecosystem.</h6></ApplyTypography>
+        element: 
+          <ApplyTypography className='md:typography-h5:text-center max-w-screen-lg'>
+            <h5>Open the door to a universe of digital assets with the Lux Card, serving as your key to effortless transactions and proficient asset management within the Lux ecosystem.</h5>
+          </ApplyTypography>
       }
     ],
-    video: {
+    video: fireworks, /*{
       blockType: 'video',
       videoProps: DEF_VIDEO_PROPS, 
-      poster: '/assets/video/card-reader-poster-comp.png',
-      sources: ['/assets/video/card-reader-md.mp4'],
+      //poster: '/assets/video/card-reader-poster-comp.png',
+      //sources: ['/assets/video/card-reader-md.mp4'],
       dim: {md: {w: 1920, h: 1080}},
-    }
+    } */
   },
   {
     blockType: 'banner-video',
@@ -141,7 +165,7 @@ export default [
       } as HeadingBlock,
       {
         blockType: 'cta',
-        specifiers: 'left',
+        specifiers: 'left 2-cols-on-mobile',
         elements: [
           {
             title: "Compare Cards",
@@ -157,11 +181,11 @@ export default [
           },
         ]
       } as CTABlock,
+      { blockType: 'space'},
       {
         blockType: 'element',
         element: <p className='text-xxs italic'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       } as ElementBlock,
-      { blockType: 'space'}
     ],
     contentRight: [
         // https://nextjs.org/docs/app/api-reference/components/image#responsive-image-with-fill
@@ -169,19 +193,38 @@ export default [
         blockType: 'image',
         src: '/assets/img/cards-1483x1361.png',
         alt: 'cards',
+        dim: {w: 1920, h: 1080},
+        fullWidthOnMobile: true,
         props: {
           sizes: '900px',
           fill: true,
-          objectFit: 'contain'
+          objectFit: 'contain',
         }
       } as ImageBlock,
     ],
-    video: {
+    video: fireworks, /*{
       blockType: 'video',
       videoProps: DEF_VIDEO_PROPS, 
       poster: '/assets/video/fireworks-poster-comp.png',
       sources: ['/assets/video/fireworks-md.mp4'],
       dim: {md: {w: 1920, h: 1080}},
-    }
+    } */
   },
+  {
+    blockType: 'banner-video',
+    specifiers: 'top left half-width',
+    content: [
+      {
+        blockType: 'heading',
+        heading: 'Apply Pay',
+        level: 1,
+        byline: 'Experience seamless Apple Wallet integration with Lux Credit. The card doubles as an events pass ' +
+          'and rewards card, complete with NFC support for seamless entry at elite Lux event and experiences.',
+        bylineLevel: 0,
+        spaceBetween: 6,
+        spaceAfter: 4,
+      } as HeadingBlock,
+    ],
+    video: fireworks, 
+  }
 ] as BannerVideoBlock[]
