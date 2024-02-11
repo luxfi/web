150c1b180aaa00e402a88fd060ae74f6a863fe07
artem ash <artemisprimedev@gmail.com>
2024-02-11 14:47:56 -0800

    new 1%: trust logos enhs

--- sites/credit/src/content/slides/new-1-percent/trust-logos.tsx
+++ sites/credit/src/content/slides/new-1-percent/trust-logos.tsx
@@ -7,23 +7,34 @@ import SVG_gda_capital from './svg/logo-gda-capital.svg'
 
 const MF = 0.6 // mobile fraction
 
+const lgclx = 'hidden lg:block'
+const smclx = 'lg:hidden'
+
+  // re wrap position: https://stackoverflow.com/a/50381928/11645689
 const TrustLogosGrid: React.FC = () => (
   <div 
     className={
-      'not-typography flex flex-row flex-wrap md:flex-nowrap justify-center items-center ' + 
-      'mt-6 gap-4 text-foreground w-full lg:mx-auto max-w-screen-lg'
+      'not-typography flex flex-row flex-wrap lg:flex-nowrap justify-center sm:px-[8vw] md:justify-between items-center ' + 
+      'mt-[5vh] lg:px-0 lg:mt-[8vh] gap-4 text-foreground w-full lg:mx-auto max-w-screen-lg'
     }
   >
-    <Image src='/assets/img/logos/logo_vera.png' width={138} height={37} alt='Vera logo' className='hidden md:block'/>
-    <Image src='/assets/img/logos/logo_vera.png' width={138 * MF} height={37 * MF} alt='Vera logo' className='md:hidden'/>
-    <SVG_cdax width={102} height={33} className='hidden md:block'/>
-    <SVG_cdax width={102 * MF} height={33 * MF} className='md:hidden'/>
-    <Image src='/assets/img/logos/logo_mc_consulting.png' alt='MC2 logo' width={178} height={28} className='hidden md:block'/>
-    <Image src='/assets/img/logos/logo_mc_consulting.png' alt='MC2 logo' width={178 * MF} height={28 * MF} className='md:hidden'/>
-    <SVG_gda_capital width={142} height={52} className='hidden md:block'/>
-    <SVG_gda_capital width={142 * MF} height={52 * MF} className='md:hidden'/>
-    <Image src='/assets/img/logos/logo_viscount.png' alt='Viscount logo' width={150} height={43} className='hidden md:block'/>
-    <Image src='/assets/img/logos/logo_viscount.png' alt='Viscount logo' width={150 * MF} height={43 * MF} className='md:hidden'/>
+    <Image src='/assets/img/logos/logo_vera.png' width={138} height={37} alt='Vera logo' className={lgclx}/>
+    <Image src='/assets/img/logos/logo_vera.png' width={138 * MF} height={37 * MF} alt='Vera logo' className={smclx}/>
+    <SVG_cdax width={102} height={33} className={lgclx}/>
+    <SVG_cdax width={102 * MF} height={33 * MF} className={smclx}/>
+    <Image src='/assets/img/logos/logo_mc_consulting.png' alt='MC2 logo' width={178} height={28} className={lgclx}/>
+    <Image 
+      src='/assets/img/logos/logo_mc_consulting.png' 
+      alt='MC2 logo' 
+      width={178 * MF} 
+      height={28 * MF} 
+      className={smclx}
+    />
+    <hr className='md:hidden' style={{ flexBasis: '100%', height: 0, margin: 0, border: 0 }} />
+    <SVG_gda_capital width={142} height={52} className={lgclx}/>
+    <SVG_gda_capital width={142 * MF} height={52 * MF} className={smclx}/>
+    <Image src='/assets/img/logos/logo_viscount.png' alt='Viscount logo' width={150} height={43} className={lgclx}/>
+    <Image src='/assets/img/logos/logo_viscount.png' alt='Viscount logo' width={150 * MF} height={43 * MF} className={smclx}/>
   </div>
 )
 
