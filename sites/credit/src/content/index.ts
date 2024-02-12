f68e3ee071853035ed36ec1406696b3a631139d3
artem ash <artemisprimedev@gmail.com>
2024-02-12 05:32:46 -0800

    credit: direct ownership: content

--- sites/credit/src/content/index.ts
+++ sites/credit/src/content/index.ts
@@ -2,8 +2,10 @@ import type { ScreenfulBlock } from '@luxdefi/ui/blocks'
 
 import new1Percent from './slides/new-1-percent'
 import keyFeatures from './slides/key-features'
+import directOwnership from './slides/direct-ownership'
 
 export default [
-  //new1Percent,
-  //keyFeatures,
+  new1Percent,
+  keyFeatures,
+  directOwnership,
 ] satisfies ScreenfulBlock[]
--- sites/credit/src/content/slides/direct-ownership.tsx
+++ sites/credit/src/content/slides/direct-ownership.tsx
@@ -9,50 +9,48 @@ import type {
   SpaceBlock,
 } from '@luxdefi/ui/blocks'
 
-import TrustLogos from './trust-logos'
-
+const byline = 'Retain complete sovereignty over your digital assets, backed by the security and transparency ' + 
+  'inherent in Lux quantum safe blockchain technology.'
 const finePrint = 'Borrow up to 50% of all your digital assets when you pay with the Lux Credit Card, ' + 
  "the most premium and exclusive card membership program around, that doesn't ever use or effect your credit score."
 
 export default {
   blockType: 'screenful',
   columnSpecifiers: ['vert-center', 'vert-center'],
-  mobileOrder: [1, 0], // mobile: card image on top please
-  footer: <TrustLogos />,
   contentColumns: [
-  [
-    {blockType: 'enh-heading', 
-      heading: {text: 'The new 1%', level: 1, mb: 6}, 
-      byline: {text: 'Never sell your crypto again with our self paying credit card', level: 6}, 
-    } satisfies EnhHeadingBlock,
-    {blockType: 'space', level: 0} satisfies SpaceBlock,
-    {
-      blockType: 'cta',
-      specifiers: 'left mobile-2-columns',
-      elements: [
-        {
-          title: "View Benefits",
-          href: "/",
-          variant: 'outline',
-        },
-        {
-          title: "Sign Up",
-          href: "/",
-          variant: 'primary',
-        },
-      ]
-    } satisfies CTABlock,
-    { blockType: 'space', level: 0},
-    {
-      blockType: 'element',
-      element: <p className='text-xxs italic'>{finePrint}</p>
-    } satisfies ElementBlock,
-  ] , [
-      // https://nextjs.org/docs/app/api-reference/components/image#responsive-image-with-fill
+    [
+      {blockType: 'enh-heading', 
+        heading: {text: 'Direct Ownership of Digital Assets', level: 1, mb: 6}, 
+        byline: {text: byline, level: 6}, 
+      } satisfies EnhHeadingBlock,
+      {blockType: 'space', level: 0} satisfies SpaceBlock,
+      {
+        blockType: 'cta',
+        specifiers: 'left mobile-2-columns',
+        elements: [
+          {
+            title: "Compare Cards",
+            href: "/",
+            variant: 'outline',
+          },
+          {
+            title: "Founder Card",
+            href: "/",
+            variant: 'primary',
+          },
+        ]
+      } satisfies CTABlock,
+      { blockType: 'space', level: 0},
+      {
+        blockType: 'element',
+        element: <p className='text-xxs italic'>{finePrint}</p>
+      },
+    ], 
+    [
     {blockType: 'image',
-      src: '/assets/img/cards-1483x1074.png',
+      src: '/assets/img/dual-elite-cards-1475x1220.png',
       alt: 'cards',
-      dim: {w: 1483, h: 1074},
+      dim: {w: 1475, h: 1220},
       fullWidthOnMobile: true,
       props: {
         sizes: '900px, 440px', 
@@ -61,9 +59,9 @@ export default {
           height: 'auto'
         }
       }
-    },
+    } satisfies ImageBlock,
     {blockType: 'element',
-      element: <p className='w-full text-sm text-muted-1 !text-right pr-2 max-w-[70vw] mx-auto md:max-w-full md:mx-0 md:pr-[5vw] relative -top-[2vh] lg:-top-[4vh]'>Black Card</p>
-    } satisfies ElementBlock
-  ]], 
+      element: <p className='w-full text-sm text-muted-1 !text-right pr-2 max-w-[70vw] mx-auto md:max-w-full md:mx-0 md:pr-[5vw] relative -top-[2vh] lg:-top-[4vh]'>Founder Card</p>
+    },
+  ], ], 
 } satisfies ScreenfulBlock
