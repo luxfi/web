2edbead3643bae488ab21e29739e92b2732891ce
erikrakuscek <erik.rakuscek@gmail.com>
2024-02-22 13:38:26 +0100

    updated auth on exchange

--- sites/exchange/src/app/layout.tsx
+++ sites/exchange/src/app/layout.tsx
@@ -2,17 +2,25 @@ import React, { type PropsWithChildren } from 'react'
 
 import { default as RootLayoutCommon, viewport } from '@hanzo/ui/next/root-layout'
 import '@hanzo/ui/style/globals.css'
+import { AuthServiceProvider } from '@hanzo/auth/service/AuthContext'
+import { getCurrentUser, type UserRecord } from '@hanzo/auth/lib/firebase/firebase-admin'
 
 import siteDef from '../siteDef'
 import metadata from '../metadata'
 
-const RootLayout: React.FC<PropsWithChildren> = ({
+const RootLayout: React.FC<PropsWithChildren> = async ({
   children
-}) => (
-  <RootLayoutCommon siteDef={siteDef}>
-    {children}
-  </RootLayoutCommon>
-)
+}) => {
+  const currentUser = await getCurrentUser()
+
+  return (
+    <AuthServiceProvider user={currentUser ? currentUser.toJSON() as UserRecord : null}>
+      <RootLayoutCommon siteDef={siteDef}>
+        {children}
+      </RootLayoutCommon>
+    </AuthServiceProvider>
+  )
+}
 
 export {
   RootLayout as default,
--- sites/exchange/src/app/page.tsx
+++ sites/exchange/src/app/page.tsx
@@ -1,46 +1,36 @@
-'use client'
-
-import React, { useEffect, useState }  from 'react'
+import React from 'react'
 import { Footer } from '@hanzo/ui/common'
 import { Main } from '@hanzo/ui/primitives'
 
 import siteDef from '../siteDef'
 import { ScreenfulBlockComponent as Screenful, type EnhHeadingBlock, type ScreenfulBlock, type ElementBlock, type SpaceBlock, type Block } from '@hanzo/ui/blocks'
 import AccessCodeInput from '@/components/AccessCodeInput'
-import {observer} from 'mobx-react'
-import { auth } from '@/service/firebase'
-import { onAuthStateChanged } from 'firebase/auth'
+import { isUserAuthenticated } from '@hanzo/auth/lib/firebase/firebase-admin'
 
-const LandingPage = observer(() => {
-  const [content, setContent] = useState<Block[]>([
+const Home = async () => {
+  let content: Block[] = [
     {blockType: 'enh-heading',
       specifiers: 'byline-center',
       heading: { text: 'For the real ones.', mb: 6 },
       byline: { text: 'Find and trade tokens, NFTs, and real world assets (RWAs)' },
     } as EnhHeadingBlock
-  ])
+  ]
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setContent([
-          {blockType: 'enh-heading',
-            specifiers: 'byline-center',
-            heading: { text: 'For the real ones.', mb: 6 },
-            byline: { text: 'Find and trade tokens, NFTs, and real world assets (RWAs)' },
-          } as EnhHeadingBlock,
-          {blockType: 'space', level: 1} as SpaceBlock,
-          {blockType: 'enh-heading',
-            specifiers: 'center',
-            heading: { text: 'Enter your access code', level: 5 },
-          } as EnhHeadingBlock,
-          {blockType: 'element', element: <AccessCodeInput/>} as ElementBlock
-        ])
-      }
-    })
-
-    return () => unsubscribe();
-  }, [auth])
+  if (await isUserAuthenticated()) {
+    content = [
+      {blockType: 'enh-heading',
+        specifiers: 'byline-center',
+        heading: { text: 'For the real ones.', mb: 6 },
+        byline: { text: 'Find and trade tokens, NFTs, and real world assets (RWAs)' },
+      } as EnhHeadingBlock,
+      {blockType: 'space', level: 1} as SpaceBlock,
+      {blockType: 'enh-heading',
+        specifiers: 'center',
+        heading: { text: 'Enter your access code', level: 5 },
+      } as EnhHeadingBlock,
+      {blockType: 'element', element: <AccessCodeInput/>} as ElementBlock
+    ]
+  }
 
   return (<>
     <Main className='md:flex-row md:gap-4 w-full'>
@@ -53,5 +43,6 @@ const LandingPage = observer(() => {
     </Main>
     <Footer siteDef={siteDef} className='max-w-screen-2xl w-full pt-16 lg:mx-auto ' />
   </>)
-})
-export default LandingPage
+}
+
+export default Home
