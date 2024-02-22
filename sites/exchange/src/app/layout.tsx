500a0da5bc7e861b5865b057fe10470e9d9b2b0e
erikrakuscek <erik.rakuscek@gmail.com>
2024-02-22 22:16:30 +0100

    moved login component to package

--- sites/exchange/src/app/layout.tsx
+++ sites/exchange/src/app/layout.tsx
@@ -3,7 +3,7 @@ import React, { type PropsWithChildren } from 'react'
 import { default as RootLayoutCommon, viewport as _viewport } from '@hanzo/ui/next/root-layout'
 import '@hanzo/ui/style/globals.css'
 import { AuthServiceProvider } from '@hanzo/auth/service/AuthContext'
-import { getCurrentUser, type UserRecord } from '@hanzo/auth/lib/firebase/firebase-admin'
+import { getCurrentUser } from '@hanzo/auth/lib/firebase/firebase-admin'
 
 import siteDef from '../siteDef'
 import _metadata from '../metadata'
@@ -22,7 +22,7 @@ const RootLayout: React.FC<PropsWithChildren> = async ({
   const currentUser = await getCurrentUser()
 
   return (
-    <AuthServiceProvider user={currentUser ? currentUser.toJSON() as UserRecord : null}>
+    <AuthServiceProvider user={currentUser?.email ? {email: currentUser?.email} : null}>
       <RootLayoutCommon siteDef={siteDef}>
         {children}
       </RootLayoutCommon>
