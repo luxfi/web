eabe8b096925f3a35cc62a5f283ab97f76802fb2
Artem Ash <633467+artemis-prime@users.noreply.github.com>
2024-03-02 18:50:51 -0800

    Market; enh buy layout and updates to cmmc 1.0.12 improvements (#41)
    * updated to cmmc@1.0.12 improvements

--- sites/market/src/app/buy/page.tsx
+++ sites/market/src/app/buy/page.tsx
@@ -6,7 +6,6 @@ import React, {
 } from 'react'
 import { observer } from 'mobx-react-lite'
 
-
 import { Skeleton } from '@hanzo/ui/primitives'
 import { cn } from '@hanzo/ui/util'
 
@@ -21,32 +20,31 @@ import {
 import CartDrawer from '@/components/cart-drawer'
 import siteDef from '@/siteDef'
 
-
 type Props = {
   searchParams?: { [key: string]: string | string[] | undefined }
 }
 
-
-
-const BuyPage: React.FC<Props> = observer(({ searchParams }) => {
+const BuyPage: React.FC<Props> = ({ searchParams }) => {
 
   const cmmc = useCommerce() 
 
   const [loading, setLoading] = useState<boolean>(true)
+
     // https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
-    // useSeachParams is called by a library we use, shoudl be within in Suspence boundary
-  const { category, message, getMutator } = useSkuAndFacetParams(setLoading)  
+    // useSeachParams is called by a library we use to impl this hook.
+    // So this component should always be within in Suspense boundary.
+  const { message, getMutator } = useSkuAndFacetParams(setLoading)  
 
   const mobile = (searchParams?.agent === 'phone')
 
-  const FacetsDesc: React.FC<PropsWithChildren & {className?: string}> = ({
+  const FacetsArea: React.FC<PropsWithChildren & {className?: string}> = ({
     children,
     className=''
   }) => {
 
-    const widgetClx = 'flex flex-row justify-between sm:gap-x-6 xs:gap-x-2 items-start'  
-    const facets1Clx = 'grid grid-cols-2 gap-0 '  + (mobile ? '' : 'pr-6 ')
-    const facets2Clx = 'grid grid-cols-4 gap-0 '
+    const widgetClx = 'flex flex-row justify-start md:justify-between lg:justify-start sm:gap-x-4 xs:gap-x-2 items-start'  
+    const facets1Clx = 'grid grid-cols-2 gap-0 '  + (mobile ? '' : '')
+    const facets2Clx = 'grid grid-cols-3 gap-0 '
 
     return !loading ? (
       <FacetsWidget
@@ -76,62 +74,62 @@ const BuyPage: React.FC<Props> = observer(({ searchParams }) => {
     )
   }
 
-  const Stage: React.FC<{className?: string}> = ({
+  const Stage: React.FC<{className?: string}> = observer(({
     className=''
-  }) => ( message ? (
+  }) => ( message || !cmmc.specifiedCategories ? (
 
       <div className={cn('typography lg:min-w-[400px] lg:max-w-[600px] overflow-hidden bg-level-1 h-[50vh] rounded-xl p-6', className)} >
-        <h5 className='text-accent text-center'>{message}</h5>
+        <h5 className='text-accent text-center'>{message ?? 'Please select an options from each group above.'}</h5>
       </div>
     ) : (
       <SelectItemInCategoryView 
         className={className} 
         mobile={mobile} 
-        category={category!}
+        category={cmmc.specifiedCategories[0]}
         lineItemRef={cmmc /* ...conveniently. :) */ }
         handleItemSelected={cmmc.setCurrentItem.bind(cmmc)}
         isLoading={loading}
       />
     ) 
-  )
+  ))
 
-  const cartColumnClx = 'hidden md:block min-w-[300px] md:min-w-[320px] ' +
-    'lg:min-w-[320px] lg:max-w-[360px] xl:min-w-[360px]'
+  const cartColumnClx = 'hidden md:block min-w-[300px] ' +
+    'lg:min-w-[340px] xl:min-w-[360px]'
 
   return mobile ? (
     <div /* id='SCV_OUTERMOST' */ className='flex flex-col justify-start items-stretch relative w-full' >
       <div /* id='SCV_FACET_CONTAINER_COMPACT' */ className='py-2 bg-background w-full sticky top-[44px]'>
-        <FacetsDesc className='sm:w-full ' >
+        <FacetsArea className='sm:w-full ' >
           <CartDrawer isMobile={true} className='md:hidden pr-1 text-primary relative' buttonClassName='h-9' >
             <Cart isMobile={true} className='p-0 border-none mt-12'/>
           </CartDrawer>
-        </FacetsDesc>   
+        </FacetsArea>   
       </div>
       <Stage />
     </div>
   ) : (
     <div /* id='SCV_OUTERMOST' */ className='flex flex-col justify-start items-stretch relative w-full' >
-      <div /* id='SCV_FACET_CONTAINER_COMPACT' */ className='xl:hidden py-2 bg-background w-full sticky top-[80px] sm:top-[44px] z-40 '>
-        <FacetsDesc className='sm:w-full' >
+      <div /* id='SCV_FACET_CONTAINER_COMPACT' */ className='lg:hidden py-2 bg-background w-full sticky top-[80px] sm:top-[44px] z-40 '>
+        <FacetsArea className='sm:w-full' >
           <CartDrawer className='md:hidden pr-1 text-primary relative' buttonClassName='h-9' >
             <Cart isMobile={mobile} className='p-0 border-none mt-12'/>
           </CartDrawer>
-        </FacetsDesc>   
+        </FacetsArea>   
       </div>
       <div /* id='SCV_COL_CONTAINER' */ className='flex flex-row justify-start gap-6 items-stretch relative h-full pt-3'>
         <div /* id='SCV_STAGE_COL' */ className='grow flex flex-col h-full relative'>
-          <div /* id='SCV_FACET_CONTAINER_BIG' */ className='sticky top-[80px] z-30 bg-background pb-2 hidden xl:flex flex-col justify-start mb-6'>
-            <FacetsDesc className='' />   
+          <div /* id='SCV_FACET_CONTAINER_BIG' */ className='sticky top-[80px] z-30 bg-background pb-2 hidden lg:flex flex-col justify-start mb-6'>
+            <FacetsArea className='' />   
           </div>
           <Stage />
         </div>
         <div /* id='SCV_CART_COLUMN' */ className={cn('z-30',  cartColumnClx)}>
-          <StoreCart className='sticky z-30 top-[146px] xl:top-[80px]' />
+          <StoreCart className='sticky z-30 top-[146px] lg:top-[80px]' />
         </div>
       </div> 
     </div>
   )
-}) 
+} 
 
 const BuyPageWrapper: React.FC<Props> = ({ searchParams }) => (
 
@@ -139,6 +137,5 @@ const BuyPageWrapper: React.FC<Props> = ({ searchParams }) => (
     <BuyPage searchParams={searchParams} />
   </Suspense>
 )
-
 
 export default BuyPageWrapper
