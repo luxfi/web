c53480327d0fba35ba620cc9655e4f053a08439e
artem ash <artemisprimedev@gmail.com>
2024-03-06 11:51:34 -0800

    moved products from /content into /conf/commerce

--- sites/credit/src/content/index.ts
+++ sites/credit/src/content/index.ts
@@ -7,10 +7,6 @@ import cashBack from './slides/cash-back'
 import flexibleBorrowing from './slides/flexible-borrowing'
 import compareTable from './slides/compare-table'
 
-import black from './products/black'
-import elite from './products/elite'
-import founder from './products/founder'
-import sovereign from './products/sovereign'
 import easySpending from './slides/easy-spending'
 import roadmap from './slides/roadmap'
 import browseCards from './slides/browse-cards'
@@ -31,14 +27,7 @@ const tiles = [
   footer
 ] satisfies ScreenfulBlock[] as ScreenfulBlock[]
 
-const products = {
-  black,
-  elite,
-  founder,
-  sovereign
-} 
 
 export {
   tiles,
-  products
 }
