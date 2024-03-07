1f58e02b8f66615d8642c978536ae35a89355a2e
artem ash <artemisprimedev@gmail.com>
2024-03-07 11:31:11 -0800

    enh: added facet images

--- sites/credit/src/conf/commerce/facets/index.tsx
+++ sites/credit/src/conf/commerce/facets/index.tsx
@@ -5,31 +5,37 @@ import SVG_Coin from './img/coin.svg'
 //import SVG_Multibar from './img/multibar.svg'
 import SVG_GD from './img/good-delivery.svg'
 
-const BLACK_IMAGE = '/assets/img/black-flat-578x373.png'
-const ELITE_IMAGE = '/assets/img/cards-dual-gd-sl-1485x1114.png'
-const FOUNDER_IMAGE = '/assets/img/cards-dual-chr-psm-1475x1220.png'
-const SOVEREIGN_IMAGE = '/assets/img/cards-2-flat-tit-1593x1231.png'
+const BLACK_IMAGE = '/assets/img/cards-flat/black-f-700x441.png'
+const ELITE_IMAGE = '/assets/img/cards-flat/gold-f-700x442.png'
+const FOUNDER_IMAGE = '/assets/img/cards-flat/chrome-cool-f-700x441.png'
+const SOVEREIGN_IMAGE = '/assets/img/cards-flat/titanium-f-700x441.png'
+
+const CC_AR = 1.58577251 // international spec for CC's and our images
 
 const TYPE = [
   {
     value: 'B',
     label: 'Black',
-    //img: BLACK_IMAGE
+    img: BLACK_IMAGE,
+    imgAR: CC_AR
   },
   {
     value: 'E',
     label: 'Elite',
-    //img: ELITE_IMAGE
+    img: ELITE_IMAGE,
+    imgAR: CC_AR,
   },
   {
     value: 'F',
     label: 'Founder',
-    //img: FOUNDER_IMAGE
+    img: FOUNDER_IMAGE,
+    imgAR: CC_AR,
   },
   {
     value: 'S',
     label: 'Sovereign',
-    //img: SOVEREIGN_IMAGE
+    img: SOVEREIGN_IMAGE,
+    imgAR: CC_AR,
   },
 ] satisfies FacetValueDesc[]
 
