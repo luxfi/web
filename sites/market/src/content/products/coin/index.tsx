03d26b3a5af77bfddc061182c06620f6bc7576b2
artem ash <artemisprimedev@gmail.com>
2024-03-17 21:24:12 -0700

    updated versions

--- sites/market/src/content/products/coin/index.tsx
+++ sites/market/src/content/products/coin/index.tsx
@@ -5,6 +5,8 @@ import video from './video'
 import accordian from './accordian'
 import Link from 'next/link'
 
+import { BuyItemButton } from '@hanzo/commerce'
+
 export default {
   blockType: 'product-detail',
   title: 'LUX COIN',
@@ -20,5 +22,10 @@ export default {
           variant: 'outline'
       }]
     } satisfies C.CTABlock as C.Block,
+    {blockType: 'element',
+      element: <div className='flex flex-col justify-center items-center gap-8'>
+        <BuyItemButton skuPath='LXM-CN' popupClx='w-[340px]' className='lg:min-w-pr-65 min-w-pr-65'>Buy</BuyItemButton>
+      </div>,
+    } satisfies C.ElementBlock as C.Block,
   ]
 } as ProductDetailBlock
