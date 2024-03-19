8df2be377c636e87c86052478a00878863ccbf50
erikrakuscek <erik.rakuscek@gmail.com>
2024-03-19 12:21:02 +0100

    remove pathname from Scripts component

--- packages/common/components/scripts.tsx
+++ packages/common/components/scripts.tsx
@@ -1,16 +1,14 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
 import { FacebookPixel } from '../next/analytics/pixel-analytics'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { useEffect } from 'react'
 
 const Scripts = () => {
-  const pathname = usePathname()
 
   useEffect(() => {
     document.body.classList.remove('loading-screen')
-  }, [pathname])
+  }, [])
 
   return (
     <>
