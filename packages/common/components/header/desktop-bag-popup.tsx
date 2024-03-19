c2e393624321b0f6c427bf110cc466cba02170c7
Erik Rakušček <erik.rakuscek@gmail.com>
2024-03-20 00:05:07 +0100

    added checkoutOpen and bagOpen states to desktop and mobile bag drawer (#106)
    Co-authored-by: artem ash <artemisprimedev@gmail.com>

--- packages/common/components/header/desktop-bag-popup.tsx
+++ packages/common/components/header/desktop-bag-popup.tsx
@@ -11,7 +11,7 @@ import {
 } from "@hanzo/ui/primitives"
 
 import { cn } from '@hanzo/ui/util'
-import { CartPanel } from '@hanzo/commerce'
+import { CartPanel, CheckoutPanel } from '@hanzo/commerce'
 
 import BagIcon from './bag-icon'
 
@@ -27,29 +27,39 @@ const DesktopBagPopup: React.FC<PropsWithChildren & {
   trigger
 }) => {
   
-  const [open, setOpen] = useState<boolean>(false)
+  const [bagOpen, setBagOpen] = useState<boolean>(false)
+  const [checkoutOpen, setCheckoutOpen] = useState<boolean>(false)
+  /* TODO: This is a hackish fix for bug with multiple dialog opened at the same time.
+  *  Needs refactor with context or so.
+  **/
 
   const _setOpen = (b: boolean) => {
     console.log("SET OPEN: ", b)
-    setOpen(b)
+    setBagOpen(b)
   }
 
   return (
-    <Popover open={open} onOpenChange={_setOpen}>
-      <PopoverTrigger className={triggerClx}>
-        {trigger}
-      </PopoverTrigger>
-      <PopoverContent sideOffset={28} className={cn('relative  flex flex-col p-0 px-4 pb-4 pt-2', popupClx)}>
-        <PopoverClose className='absolute z-20 right-2 top-2 self-end hover:bg-level-3 text-muted hover:text-accent p-1 rounded-full'><X className='w-5 h-5'/></PopoverClose>
-        <CartPanel className='mt-4 w-full border-none py-0 px-4'>
-          <div className='flex items-center justify-center'>
-            <BagIcon width={32} height={32} className='fill-foreground mr-2 relative -top-1'/>
-            <p className='font-nav text-default'>Your Bag</p>
-          </div>
-          <div className='h-[1px] w-full mb-4 mt-3 bg-muted-3'/>
-        </CartPanel>
-      </PopoverContent>
-    </Popover>
+    <>
+      <Popover open={bagOpen} onOpenChange={_setOpen}>
+        <PopoverTrigger className={triggerClx}>
+          {trigger}
+        </PopoverTrigger>
+        <PopoverContent sideOffset={28} className={cn('relative  flex flex-col p-0 px-4 pb-4 pt-2', popupClx)}>
+          <PopoverClose className='absolute z-20 right-2 top-2 self-end hover:bg-level-3 text-muted hover:text-accent p-1 rounded-full'><X className='w-5 h-5'/></PopoverClose>
+          <CartPanel
+            className='mt-4 w-full border-none py-0 px-4'
+            onCheckoutOpen={() => setCheckoutOpen(true)}
+          >
+            <div className='flex items-center justify-center'>
+              <BagIcon width={32} height={32} className='fill-foreground mr-2 relative -top-1'/>
+              <p className='font-nav text-default'>Your Bag</p>
+            </div>
+            <div className='h-[1px] w-full mb-4 mt-3 bg-muted-3'/>
+          </CartPanel>
+        </PopoverContent>
+      </Popover>
+      <CheckoutPanel open={checkoutOpen} onCheckoutClose={() => setCheckoutOpen(false)}/>
+    </>
   )
 }
 
--- packages/common/components/header/mobile-bag-drawer.tsx
+++ packages/common/components/header/mobile-bag-drawer.tsx
@@ -8,7 +8,7 @@ import {
 } from '@hanzo/ui/primitives'
 
 import { cn } from '@hanzo/ui/util'
-import { CartPanel } from '@hanzo/commerce'
+import { CartPanel, CheckoutPanel } from '@hanzo/commerce'
 
 import BagIcon from './bag-icon'
 
@@ -19,24 +19,45 @@ const MobileBagDrawer: React.FC<{
   trigger,
   className='',
 }) => {
+  const [bagOpen, setBagOpen] = useState<boolean>(false)
+  const [checkoutOpen, setCheckoutOpen] = useState<boolean>(false)
+  /* TODO: This is a hackish fix for bug with multiple dialog opened at the same time.
+  *  Needs refactor with context or so.
+  **/
+
+  const onCheckoutOpen = () => {
+    setBagOpen(false)
+    setCheckoutOpen(true)
+  }
+
+  const onCheckoutClose = () => {
+    setBagOpen(true)
+    setCheckoutOpen(false)
+  }
 
   return (
-    <Drawer >
-      <DrawerTrigger className={className}>
-        {trigger}
-      </DrawerTrigger>
-      <DrawerContent 
-        className={cn('rounded-tl-xl rounded-tr-xl p-0 overflow-hidden')}
-      >
-        <CartPanel className=' mt-4 mb-8 border-none py-0 px-4 w-full sm:min-w-[350px] sm:max-w-[550px] min-h-[60vh]'>
-          <div className='flex items-center justify-center'>
-            <BagIcon width={32} height={32} className='fill-foreground mr-2 relative -top-1'/>
-            <p className='font-nav text-default'>Your Bag</p>
-          </div>
-          <div className='h-[1px] w-full mb-4 mt-3 bg-muted-3'/>
-        </CartPanel>
-      </DrawerContent>
-    </Drawer>
+    <>
+      <Drawer open={bagOpen} onOpenChange={setBagOpen}>
+        <DrawerTrigger className={className}>
+          {trigger}
+        </DrawerTrigger>
+        <DrawerContent 
+          className={cn('rounded-tl-xl rounded-tr-xl p-0 overflow-hidden')}
+        >
+          <CartPanel
+            className=' mt-4 mb-8 border-none py-0 px-4 w-full sm:min-w-[350px] sm:max-w-[550px] min-h-[60vh]'
+            onCheckoutOpen={onCheckoutOpen}
+          >
+            <div className='flex items-center justify-center'>
+              <BagIcon width={32} height={32} className='fill-foreground mr-2 relative -top-1'/>
+              <p className='font-nav text-default'>Your Bag</p>
+            </div>
+            <div className='h-[1px] w-full mb-4 mt-3 bg-muted-3'/>
+          </CartPanel>
+        </DrawerContent>
+      </Drawer>
+      <CheckoutPanel open={checkoutOpen} onCheckoutClose={onCheckoutClose}/>
+    </>
   )
 }
 
