e55107e225d9fcc2e369b46c27deb1447cfccef5
erikrakuscek <erik.rakuscek@gmail.com>
2024-03-25 08:56:12 +0100

    fix merge issues

--- sites/credit/src/app/compare/page.tsx
+++ sites/credit/src/app/compare/page.tsx
@@ -3,8 +3,6 @@ import React  from 'react'
 import '@/blocks/registerComponents'
 
 import CompareCards from '@/components/compare-cards'
-import { Footer } from '@luxdefi/common'
-import siteDef from '@/site-def'
 
 type Props = {
   searchParams?: { [key: string]: string | string[] | undefined }
@@ -16,10 +14,7 @@ const Page = ({ searchParams }: Props ) => {
   const agent = searchParams?.agent as string
 
   return (
-    <>
-      <CompareCards /> 
-      <Footer siteDef={siteDef} className='max-w-screen-2xl w-full pt-16 lg:mx-auto ' />
-    </> 
+    <CompareCards /> 
   )
 }
 
