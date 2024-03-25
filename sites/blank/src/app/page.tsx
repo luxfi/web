d529616eb3ceb4ae60181f758693aba62dc8ab25
artem ash <artemisprimedev@gmail.com>
2024-03-24 17:21:10 -0700

    Yet another

--- sites/blank/src/app/page.tsx
+++ sites/blank/src/app/page.tsx
@@ -2,6 +2,8 @@ import React  from 'react'
 import { Footer } from '@luxdefi/common'
 import { ApplyTypography, Main } from '@hanzo/ui/primitives'
 
+import SheetTest from '@/components/sheet-test'
+
 /* for example, as slug
 type Props = {
   params: { slug: string }
@@ -12,10 +14,7 @@ import siteDef from '../site-def'
 
 const UniversalPage = (/* { params, searchParams }: Props */) => (<>
   <Main className='md:flex-row md:gap-4 '>
-    <ApplyTypography>
-      <h1>Empty Page</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </ApplyTypography>
+    <SheetTest />
   </Main>
   <Footer siteDef={siteDef} className='max-w-screen-2xl w-full pt-16 lg:mx-auto ' />
 </>)
--- sites/credit/src/content/slides/new-1-percent/index.tsx
+++ sites/credit/src/content/slides/new-1-percent/index.tsx
@@ -21,7 +21,7 @@ export default {blockType: 'screenful',
       } satisfies EnhHeadingBlock as Block,
       {blockType: 'space', level: 1} satisfies SpaceBlock as Block,
       {blockType: 'item-ctas',
-        otherLink: { title: "Compare", href: "/compare", variant: 'outline' },
+        otherLink: { title: "TEST", href: "/test", variant: 'outline' },
         skuPath: 'LXM-CR-B'
       } satisfies ItemCTASBlock as Block,
     ], 
