b1f0d27a3f0bd23aa8825466e81369098dcd741d
artem ash <artemisprimedev@gmail.com>
2024-04-05 20:43:03 -0700

    market/test: form for cmmc.peekAtNode()

---
+++ sites/market/src/app/test/page.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+import { cn } from '@hanzo/ui/util'
+import {
+  Main,
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input
+} from '@hanzo/ui/primitives'
+import { useCommerce } from '@hanzo/commerce'
+
+type Props = {
+  searchParams?: { [key: string]: string | string[] | undefined }
+}
+
+const FormSchema = z.object({
+  skupath: z.string().min(2, {
+    message: 'sku path must be at least 2 characters.',
+  }),
+})
+
+const InputForm: React.FC<{
+  onSubmit: (data: z.infer<typeof FormSchema>) => void
+  className?: string
+}> = ({
+  onSubmit,
+  className
+}) => {
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      skupath: '',
+    },
+  })
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className={className}>
+        <FormField
+          control={form.control}
+          name='skupath'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>sku path</FormLabel>
+              <FormControl>
+                <Input placeholder='LXM-CR' {...field} className='border' />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+const Page = ({ searchParams }: Props ) => {
+
+  const cmmc = useCommerce()
+
+  const onSubmit = (data: z.infer<typeof FormSchema>): void => {
+    console.log('SKU PATH: ' + data.skupath)
+    const result = cmmc.peekAtNode(data.skupath)
+    const toDisplay = {
+      role: result.role,
+      family: result.family ? result.family.id : 'UNDEF',
+      families: result.families ? result.families.map((f) => (f.id)) : 'UNDEF',
+      node: result.node ? (result.node.skuToken + ': ' + result.node.label) : 'UNDEF'
+    }
+    console.log(toDisplay)
+  }
+
+  return (
+    <Main className='w-full h-[90vh] pt-10'>
+      <InputForm onSubmit={onSubmit} className='mx-auto w-[400px] space-y-6'/>
+    </Main>
+  )
+}
+
+export default Page
