1267af20a0e13ce328c411a7f998f205c0e50eb0
erikrakuscek <erik.rakuscek@gmail.com>
2024-04-15 14:41:55 +0200

    fixed footer typography

--- sites/credit/src/components/footer-slide.tsx
+++ sites/credit/src/components/footer-slide.tsx
@@ -17,15 +17,15 @@ const FooterSlide: React.FC<{
   const swipeInnerTouch = swipeInner + 'h-[100svh] '
 
   return (<>
-    <div className='hidden md:flex'>
-      <ApplyTypography className={cn(swipeOuter, 'swipe flex flex-col justify-between items-stretch pt-20 gap-3')} >
-        <div className='flex flex-col gap-1 max-w-screen-md m-auto items-center'>
+    <div className='hidden md:flex w-full'>
+      <div className={cn(swipeOuter, 'swipe flex flex-col justify-between items-stretch pt-20 gap-3 w-full')} >
+        <ApplyTypography className='flex flex-col !gap-2 max-w-screen-md m-auto items-center'>
           <h3 className='!text-center'>ALL CARDS</h3>
           <p className='text-sm !text-center'>Join the financial revolution with Lux Credit Card, and make your transactions not just purchases, but investments in your legacy, thanks to our zero-interest credit facility with automatic loan repayment.</p>
-        </div>
-        <CardsCarousel className='hidden md:flex' cardClx='lg:basis-1/5 xl:basis-[14.2857143%]'/>
+        </ApplyTypography>
+        <CardsCarousel className='hidden md:flex w-full' cardClx='lg:px-12 xl:px-16 2xl:px-20'/>
         <Footer siteDef={siteDef} className='grow-0 max-w-screen-2xl w-full lg:mx-auto sm:pt-6 border-t-0 flex flex-col justify-between md:justify-start'/>
-      </ApplyTypography>
+      </div>
     </div>
     <div className='md:hidden'>
       <div className={swipeOuter}>
