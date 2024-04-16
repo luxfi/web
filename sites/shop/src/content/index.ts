a676556a10ef81b70688c1d3ab8685b0c562fe83
erikrakuscek <erik.rakuscek@gmail.com>
2024-04-16 11:19:10 +0200

    market, shop: reenabled uranium page

--- sites/shop/src/content/index.ts
+++ sites/shop/src/content/index.ts
@@ -6,7 +6,7 @@ import coin from './products/coin'
 //import credit from './products/credit'
 import validator from './products/validator'
 import pass from './products/pass'
-// import uranium from './products/uranium'
+import uranium from './products/uranium'
 
 const products = {
   silver,
@@ -15,7 +15,7 @@ const products = {
   //credit,
   validator,
   pass,
-  // uranium,
+  uranium,
 }
 
 export {
