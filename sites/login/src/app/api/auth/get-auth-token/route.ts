ec471b153a93afadfb3ded1a407d41ca83279c2b
erikrakuscek <erik.rakuscek@gmail.com>
2024-04-19 16:33:44 +0200

    login: added options route handler

--- sites/login/src/app/api/auth/get-auth-token/route.ts
+++ sites/login/src/app/api/auth/get-auth-token/route.ts
@@ -23,3 +23,19 @@ export async function GET(request: NextRequest) {
     },
   })
 }
+
+export async function OPTIONS(request: NextRequest) {
+  const token = cookies().get('auth-token')
+  const reqToken = request.cookies.get('auth-token')?.value
+  const origin = request.headers.get('origin') ?? ''
+
+  return new Response(JSON.stringify({ token: token, reqToken: reqToken }), {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': domains.includes(origin) ? origin : '',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Credentials': 'true', // Allow credentials
+    },
+  })
+}
