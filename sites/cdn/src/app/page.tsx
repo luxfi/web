f8658016fbec7264137bcc11bc5e4c3e0c291dd6
Zach Kelling <z@zeekay.io>
2024-06-04 04:57:23 +0800

    Update to latest @hanzo/ui

--- sites/cdn/src/app/page.tsx
+++ sites/cdn/src/app/page.tsx
@@ -1,24 +1,47 @@
+'use client'
+
 import React  from 'react'
-import { Footer } from '@luxfi/core'
-import { ApplyTypography, Main } from '@hanzo/ui/primitives'
+import { observer } from 'mobx-react-lite'
 
+import { ApplyTypography } from '@hanzo/ui/primitives'
+import { useAuth } from '@hanzo/auth/service'
+import { LoginPanel } from '@hanzo/auth/components'
 
-/* for example, as slug
-type Props = {
-  params: { slug: string }
-  searchParams?: { [key: string]: string | string[] | undefined }
-}
-*/
+import { AccessCodeInput, Footer, Main } from '@luxfi/core'
+
 import siteDef from '../site-def'
+import { useRouter } from 'next/navigation'
 
-const UniversalPage = (/* { params, searchParams }: Props */) => (<>
-  <Main className='md:flex-row md:gap-4 '>
-    <ApplyTypography>
-      <h1>LUX CDN</h1>
-      <p>You may be searching for one of our other properties.</p>
-    </ApplyTypography>
-  </Main>
-  <Footer siteDef={siteDef} className='max-w-screen-2xl w-full pt-16 lg:mx-auto ' />
-</>)
+const Home = observer(() => {
+  const router = useRouter()
+  const auth = useAuth()
 
-export default UniversalPage
+  const validCodes = ['777777']
+
+  const redirectToApp = () => {
+    if (typeof window !== 'undefined') {
+      const { protocol, hostname } = window.location
+      const newHostname = `app.${hostname}`
+      router.push(`${protocol}//${newHostname}`)
+    }
+  }
+
+  return (<>
+    <Main className='h-screen -mt-[44px] md:-mt-[80px]'>
+      <ApplyTypography className='flex flex-col gap-8 m-auto text-center'>
+        <h1>MEMBER LOGIN</h1>
+        {auth && (auth.loggedIn ? (
+          <div className='flex flex-col gap-4 max-w-[40rem] mx-auto'>
+            <h5>If you are an existing Lux member accessing this website for the first time, please enter your invite code.</h5>
+            <AccessCodeInput validCodes={validCodes} onSuccess={redirectToApp}/>
+          </div>
+        ) : (
+          <LoginPanel noHeading className='max-w-[20rem] mx-auto' />
+        ))}
+      </ApplyTypography>
+    </Main>
+    <Footer siteDef={siteDef} className='max-w-screen-2xl w-full pt-16 lg:mx-auto ' />
+  </>)
+})
+
+export default Home
