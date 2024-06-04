7d3ee9a0bfd4fcc1f685ee838ec24cbbd54f3fa6
artem ash <artemisprimedev@gmail.com>
2024-06-04 16:26:14 -0700

    shop compiles

--- sites/shop/src/content/commerce/families/videos/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as gold } from './gold'
-export { default as silver } from './silver'
-export { default as coin } from './coin'
-export { default as key } from './key'
-export { default as validator } from './validator'
