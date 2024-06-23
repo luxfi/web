f736dd2c4c36317b3e0a6b5813b685abfb355be4
artem ash <artemisprimedev@gmail.com>
2024-06-22 19:29:46 -0700

    versions; build issues

--- sites/credit/src/app/checkout/page.tsx
+++ sites/credit/src/app/checkout/page.tsx
@@ -1,28 +1,17 @@
-'use client'
 import React from 'react'
-import { useRouter } from 'next/navigation'
 
 import { cn } from '@hanzo/ui/util'
 import { CheckoutPanel, Main } from '@luxfi/core'
 
-const Page: React.FC = () => {
-
-  const router = useRouter()
-
-  const handleCheckoutDone = () => {
-    router.back()
-  }
-
-  return (
-    <Main id='CHECKOUT_MAIN' className={cn(
-      '!px-0 !py-0',
-      'w-full h-[100vh]',
-      'animate-in md:zoom-in-90',
-      'shadow-lg bg-background'
-    )}>
-      <CheckoutPanel close={handleCheckoutDone} className='w-full h-full' />
-    </Main>
-  )
-}
+const Page: React.FC = () => (
+  <Main id='CHECKOUT_MAIN' className={cn(
+    '!px-0 !py-0',
+    'w-full h-[100vh]',
+    'animate-in md:zoom-in-90',
+    'shadow-lg bg-background'
+  )}>
+    <CheckoutPanel clx='w-full h-full' />
+  </Main>
+)
 
 export default Page
