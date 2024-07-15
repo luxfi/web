c0ae900b52d7a10cc525679702d4294ec4dac829
Zach Kelling <z@zeekay.io>
2024-07-15 10:27:54 +0800

    menu updated with hover event

--- packages/core/components/commerce/desktop-nav-menu.tsx
+++ packages/core/components/commerce/desktop-nav-menu.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import * as React from "react"
-
 import Link from "next/link"
 import { cn } from '@hanzo/ui/util'
 import type { LinkDef } from '@hanzo/ui/types'
@@ -17,64 +16,57 @@ import {
 } from '@hanzo/ui/primitives'
 import Warpcast from "../icons/warpcast"
 
-const DesktopNav: React.FC<{
-  links: LinkDefExtended[],
-}> = ({
-  links
-
-}) => (
-    links.length > 0 ? (
-      <NavigationMenu>
-        <NavigationMenuList>
-          {links.map((el, index) => {
-
-            if (el.isAIMenu) {
-              return (
-                <NavigationMenuItem key={index}>
-                  <Link href={el.href} legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                      {el.title}
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              )
-            } else if (el.title == "Credit") {
-              return (
-                <NavigationMenuItem key={index}>
-                  <NavigationMenuTrigger>{el.title}</NavigationMenuTrigger>
-                  <NavigationMenuContent className="!left-0">
-                    <div className="grid grid-cols-3 w-[846px]">
-                      {GroupChildMenu(el.childMenu)}
-                    </div>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-              )
-            } else {
-              return (
-                <NavigationMenuItem key={index}>
-                  <NavigationMenuTrigger className=" !rounded-2xl">{el.title}</NavigationMenuTrigger>
-                  <NavigationMenuContent className="!left-0">
-                    <div className="flex flex-row">
-                      {GroupChildMenu(el.childMenu)}
-                    </div>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-              )
-            }
-
-          })}
-        </NavigationMenuList>
-      </NavigationMenu>
-    ) : null
-  )
+const DesktopNav: React.FC<{ links: LinkDefExtended[] }> = ({ links }) => (
+  links.length > 0 ? (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {links.map((el, index) => {
+          if (el.isAIMenu) {
+            return (
+              <NavigationMenuItem key={index}>
+                <Link href={el.href} legacyBehavior passHref >
+                  <NavigationMenuLink className={cn('!rounded-2xl ', navigationMenuTriggerStyle())}>
+                    {el.title}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            )
+          } else if (el.title === "Cards") {
+            return (
+              <NavigationMenuItem key={index}>
+                <NavigationMenuTrigger className="!rounded-2xl">{el.title}</NavigationMenuTrigger>
+                <NavigationMenuContent className="!left-0">
+                  <div className="grid grid-cols-3 w-[846px]">
+                    <GroupChildMenu childs={el.childMenu} />
+                  </div>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            )
+          } else {
+            return (
+              <NavigationMenuItem key={index}>
+                <NavigationMenuTrigger className="!rounded-2xl">{el.title}</NavigationMenuTrigger>
+                <NavigationMenuContent className="!left-0">
+                  <div className="flex flex-row">
+                    <GroupChildMenu childs={el.childMenu} />
+                  </div>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            )
+          }
+        })}
+      </NavigationMenuList>
+    </NavigationMenu>
+  ) : null
+)
 export default DesktopNav
 
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, key, ...props }, ref) => {
+>(({ className, title, children, ...props }, ref) => {
   return (
-    <li key={key}>
+    <li>
       <NavigationMenuLink asChild>
         <a
           ref={ref}
@@ -95,11 +87,11 @@ const ListItem = React.forwardRef<
 })
 ListItem.displayName = "ListItem"
 
-const GroupChildMenu = (childs: ChildMenu[] | undefined) => {
-  // Initialize groupedChildMenus with the type specification
+const GroupChildMenu: React.FC<{ childs: ChildMenu[] | undefined }> = ({ childs }) => {
   if (!childs) {
     return null
   }
+
   let groupedChildMenus = childs.reduce((grouped: Record<string, ChildMenu[]>, childLink) => {
     if (childLink.groupName) {
       if (!grouped[childLink.groupName]) {
@@ -108,24 +100,32 @@ const GroupChildMenu = (childs: ChildMenu[] | undefined) => {
       grouped[childLink.groupName].push(childLink)
     }
     return grouped
-  }, {} as Record<string, ChildMenu[]>) // added explicit type here
+  }, {} as Record<string, ChildMenu[]>)
 
-  // Convert groups object to array
-  return Object.entries(groupedChildMenus).map(([groupName, childLinks]: [string, ChildMenu[]]) => { // added type specification here
-    return (
-      <div key={groupName} className={` py-4 px-4 ${groupName === "Elite Card" || groupName === "Sovereign Card" ? " -mt-34" : ""}` }>
-        <h2 className="text-muted-1">{groupName}</h2>
-        <ul className=" w-[200px] gap-3 md:w-[250px] lg:w-[250px]">
-          {childLinks.map((link) => (
-            <div className={"flex items-center"} key={link.title}>
-              {link.icon}
-              <ListItem key={link.title} title={link.title} href={link.href} className="text-muted-1 hover:text-primary hover:bg-transparent">
+  return Object.entries(groupedChildMenus).map(([groupName, childLinks]: [string, ChildMenu[]]) => (
+    <div key={groupName} className={`py-4 px-4 ${groupName === "Elite Card" || groupName === "Sovereign Card" ? "-mt-34" : ""}`}>
+      <h2 className="text-muted-1">{groupName}</h2>
+      <ul className="w-[200px] gap-3 md:w-[250px] lg:w-[250px]">
+        {childLinks.map((link) => (
+          <div className="flex items-center element-container text-muted-1 hover:text-primary" key={link.title}>
+            {
+              link.icon_act ? (
+                <>
+                  <div className="icon-container-nor">{link.icon}</div>
+                  <div className="icon-container-hov">{link.icon_act}</div>
+                </>
+              ):(
+                <div>{link.icon}</div>
+              )
+            }
+            <div className="text-container">
+              <ListItem key={link.title} title={link.title} href={link.href} className="hover:bg-transparent">
                 {link.contents}
               </ListItem>
             </div>
-          ))}
-        </ul>
-      </div>
-    )
-  })
+          </div>
+        ))}
+      </ul>
+    </div>
+  ))
 }
---
+++ packages/core/components/icons/bridge-act.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { type LucideProps } from 'lucide-react'
+
+const BridgeAct: React.FC<LucideProps> = (props: LucideProps) => (
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+<circle cx="14.8468" cy="8.04857" r="0.324812" stroke="white" stroke-width="0.5"/>
+<circle cx="14.4936" cy="8.04852" r="1.15655" stroke="white" stroke-width="0.5"/>
+<circle cx="13.4998" cy="8.04871" r="2.15027" stroke="white" stroke-width="0.5"/>
+<circle cx="12.4576" cy="7.92913" r="3.19256" stroke="white" stroke-width="0.5"/>
+<circle cx="11.3852" cy="7.92914" r="4.26496" stroke="white" stroke-width="0.5"/>
+<circle cx="10.0415" cy="7.92917" r="5.60862" stroke="white" stroke-width="0.5"/>
+<circle cx="8.4001" cy="7.9292" r="7.25" stroke="white" stroke-width="0.5"/>
+</svg>
+
+
+)
+
+export default BridgeAct
