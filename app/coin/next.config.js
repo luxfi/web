e8c3faa0578f47cc41c19539dddc71cdd453d876
Zach Kelling <z@zeekay.io>
2024-08-30 01:41:06 -0700

    Optimize webpack

--- app/coin/next.config.js
+++ app/coin/next.config.js
@@ -1,10 +1,50 @@
-const withMDX = require('@next/mdx')()
-const svgrWebpackConfig = require('./svgr.next.config')
+//const withMDX = require('@next/mdx')()
+//const svgrWebpackConfig = require('./svgr.next.config')
+//
+///** @type {import('next').NextConfig} */
+//const nextConfig = {
+//  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+//  reactStrictMode: true,
+//  images: {
+//    remotePatterns: [
+//      {
+//        protocol: 'https',
+//        hostname: 'img.youtube.com',
+//        pathname: '**',
+//      },
+//      {
+//        protocol: "http",
+//        hostname: "localhost",
+//      }
+//    ],
+// },
+//    // https://stackoverflow.com/questions/72621835/how-to-fix-you-may-need-an-appropriate-loader-to-handle-this-file-type-current
+//  transpilePackages: [
+//    '@hanzo/ui',
+//    '@hanzo/auth',
+//    '@hanzo/commerce',
+//    '@luxfi/ui',
+//    '@luxfi/data'
+//  ],
+//  productionBrowserSourceMaps: true,
+//  webpack: svgrWebpackConfig // if we need others, set up a chain of calls.
+//}
+//
+//module.exports = withMDX(nextConfig)
 
-/** @type {import('next').NextConfig} */
+const withMDX = require('@next/mdx')();
+const svgrWebpackConfig = require('./svgr.next.config');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const isProd = process.env.NODE_ENV === 'production';
+const isStaging = process.env.VERCEL_ENV === 'staging';
+
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
       {
@@ -13,21 +53,56 @@ const nextConfig = {
         pathname: '**',
       },
       {
-        protocol: "http",
-        hostname: "localhost",
-      }
-    ],    
- },
-    // https://stackoverflow.com/questions/72621835/how-to-fix-you-may-need-an-appropriate-loader-to-handle-this-file-type-current
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
   transpilePackages: [
-    '@hanzo/ui', 
-    '@hanzo/auth', 
-    '@hanzo/commerce', 
+    '@hanzo/ui',
+    '@hanzo/auth',
+    '@hanzo/commerce',
     '@luxfi/ui',
-    '@luxfi/data'
+    '@luxfi/data',
   ],
-  productionBrowserSourceMaps: true,
-  webpack: svgrWebpackConfig // if we need others, set up a chain of calls. 
-}
+  productionBrowserSourceMaps: process.env.ENABLE_SOURCEMAPS === 'true',
+  webpack: (config, { isServer }) => {
+    // Merge SVGR Webpack configuration
+    svgrWebpackConfig(config, { isServer });
 
-module.exports = withMDX(nextConfig)
+    // Add CSS/PostCSS configuration
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              ident: 'postcss',
+              plugins: [
+                require('postcss-import'),
+                require('tailwindcss/nesting'),
+                require('tailwindcss'),
+                require('autoprefixer'),
+              ],
+            },
+          },
+        },
+      ],
+    });
+
+    // Enable Webpack's filesystem caching for faster builds
+    config.cache = {
+      type: 'filesystem',
+      buildDependencies: {
+        config: [__filename],
+      },
+    };
+
+    return config;
+  },
+};
+
+module.exports = withBundleAnalyzer(withMDX(nextConfig));
